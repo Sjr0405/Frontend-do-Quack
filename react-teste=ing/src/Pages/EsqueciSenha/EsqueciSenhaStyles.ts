0d06d72816e5dbac0ebf6c9287f66bbbc85b7f5d
Elisson Marques <nadsonnodachi@gmail.com>
2024-11-27 16:02:25 -0300

    Update EsqueciSenhaStyles.ts

--- react-teste=ing/src/Pages/EsqueciSenha/EsqueciSenhaStyles.ts
+++ react-teste=ing/src/Pages/EsqueciSenha/EsqueciSenhaStyles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Box, Typography } from "@mui/material";
-import BackgroundImage from 'src/svgs/Cadastro-svgs/1.svg';
+import BackgroundImage from '../../svgs/Cadastro-svgs/1.svg';
 
 export const Container = styled.div`
   display: flex;
