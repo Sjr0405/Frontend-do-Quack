05559f4a395fe302d2659e71e5f4d49a5c1a3e1d
Elisson Marques <nadsonnodachi@gmail.com>
2024-11-27 15:22:51 -0300

    Update vite.config.ts

--- react-teste=ing/vite.config.ts
+++ react-teste=ing/vite.config.ts
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-       proxy: {
+    proxy: {
       '/auth': {
-        target: 'http://localhost:5030',
+        target: 'srv622492.hstgr.cloud:5030/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/auth/, '/auth')
       },
       '/users': {
-        target: 'https://srv622492.hstgr.cloud:5030',
+        target: 'srv622492.hstgr.cloud:5030',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/users/, '/users')
