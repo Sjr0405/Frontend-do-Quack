4d223f04ea4351cc40172607db4437790b8d2b09
Elisson Marques <nadsonnodachi@gmail.com>
2024-11-27 08:55:15 -0300

    alteração das rotas

--- react-teste=ing/vite.config.ts
+++ react-teste=ing/vite.config.ts
@@ -6,37 +6,37 @@ export default defineConfig({
   server: {
     proxy: {
       '/auth': {
-        target: 'https://145.223.27.98:5030/',
+        target: 'https://srv622492.hstgr.cloud:5030/',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/auth/, '/auth')
       },
       '/users': {
-        target: 'https://145.223.27.98:5030',
+        target: 'https://srv622492.hstgr.cloud:5030',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/users/, '/users')
       },
       '/tasks': {
-        target: 'https://145.223.27.98:5030',
+        target: 'https://srv622492.hstgr.cloud:5030',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/tasks/, '/tasks')
       },
       '/roadmaps': {
-        target: 'https://145.223.27.98:5030',
+        target: 'https://srv622492.hstgr.cloud:5030',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/roadmaps/, '/roadmaps')
       },
       '/achievements': {
-        target: 'https://145.223.27.98:5030',
+        target: 'https://srv622492.hstgr.cloud:5030',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/achievements/, '/achievements')
       },
       '/statistics': {
-        target: 'https://145.223.27.98:5030',
+        target: 'https://srv622492.hstgr.cloud:5030',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/statistics/, '/statistics')
