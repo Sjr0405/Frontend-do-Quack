519e113cb676ad25dea466d462c7498ffba06f7c
Elisson Marques <nadsonnodachi@gmail.com>
2024-12-02 23:43:54 -0300

    atualizações das rodas

--- react-teste=ing/vite.config.ts
+++ react-teste=ing/vite.config.ts
@@ -5,42 +5,84 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/auth': {
-        target: 'https://145.223.27.98:5030',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/auth/, '/auth')
-      },
-      '/users': {
+      '/api/users': {
         target: 'https://localhost:5030',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/users/, '/users')
+        rewrite: (path) => path.replace(/^\/api\/users/, '/api/users')
       },
-      '/tasks': {
+      '/api/users/address/update': {
         target: 'https://localhost:5030',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/tasks/, '/tasks')
+        rewrite: (path) => path.replace(/^\/api\/users\/address\/update/, '/api/users/address/update')
       },
-      '/roadmaps': {
+      '/api/users/address/create': {
         target: 'https://localhost:5030',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/roadmaps/, '/roadmaps')
+        rewrite: (path) => path.replace(/^\/api\/users\/address\/create/, '/api/users/address/create')
       },
-      '/achievements': {
+      '/api/users/achievement/create': {
         target: 'https://localhost:5030',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/achievements/, '/achievements')
+        rewrite: (path) => path.replace(/^\/api\/users\/achievement\/create/, '/api/users/achievement/create')
       },
-      '/statistics': {
+      '/api/users/address/getAll': {
         target: 'https://localhost:5030',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/statistics/, '/statistics')
+        rewrite: (path) => path.replace(/^\/api\/users\/address\/getAll/, '/api/users/address/getAll')
+      },
+      '/api/users/achievement/getAll': {
+        target: 'https://localhost:5030',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/users\/achievement\/getAll/, '/api/users/achievement/getAll')
+      },
+      '/api/users/login': {
+        target: 'https://localhost:5030',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/users\/login/, '/api/users/login')
+      },
+      '/api/users/create': {
+        target: 'https://localhost:5030',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/users\/create/, '/api/users/create')
+      },
+      '/api/tasks': {
+        target: 'https://localhost:5030',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/tasks/, '/api/tasks')
+      },
+      '/api/steps': {
+        target: 'https://localhost:5030',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/steps/, '/api/steps')
+      },
+      '/api/roadmaps': {
+        target: 'https://localhost:5030',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/roadmaps/, '/api/roadmaps')
+      },
+      '/api/roadmaps/category': {
+        target: 'https://localhost:5030',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/roadmaps\/category/, '/api/roadmaps/category')
+      },
+      '/api/achievements': {
+        target: 'https://localhost:5030',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api\/achievements/, '/api/achievements')
       }
     }
-  },
+  }
 });
